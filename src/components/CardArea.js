7f2931b8373d81d9ecce39470bc00d8d59434847
venkateshtata <venkateshtata9@gmail.com>
2021-05-24 19:39:06 +0530

    init

---
+++ src/components/CardArea.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../styles/cardArea.css';
+import ReactCardCarousel from "react-card-carousel";
+
+
+function CardArea() {
+    return (
+      <div className="container-style">
+          <div className="card-style">
+            <h1>hello</h1>
+          </div>
+      </div>
+    );
+  }
+
+export default CardArea;
