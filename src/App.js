f96a701e91e7f557ccad3da04f70f11b1bc9d3c2
rachita7363 <rachitav35@gmail.com>
2021-05-24 22:19:15 +0530

    WIP:header area initialised

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import CardArea from './components/CardArea';
+import MainHeadingArea from './components/MainHeadingArea';
 
 function App() {
   return (
     <div className="App">
+      <div className="section_1">
+        <MainHeadingArea/>
+      </div>
       <CardArea/>
     </div>
   );
---
+++ src/components/MainHeadingArea.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../styles/mainHeadingArea.css';
+import Button from 'react-bootstrap/Button'
+const MainHeadingArea = () => {
+    return (
+        <div className="main_heading_area_container">
+            <div className="main_heading_text">
+            <p>An AI enabled automaton system that lets you
+take control and saves energy   </p>
+<Button variant="success" id="header-btn" size="lg">Experience</Button>{' '}
+            </div>
+  
+        </div>
+    )
+}
+export default MainHeadingArea;
