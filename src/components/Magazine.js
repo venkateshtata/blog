6319a256b9104cdc2242daa1f5e08aa43f88ff74
venkateshtata <venkateshtata9@gmail.com>
2021-09-01 07:57:37 +0530

    added papers

---
+++ src/components/Magazine.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Magazine() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
--- src/components/Papers.js
+++ src/components/Papers.js
@@ -1,5 +1,9 @@
 import React from 'react'
 import '../styles/papers.css'
+import { AwesomeButtonSocial} from "react-awesome-button";
+import AwesomeButtonStyles from "react-awesome-button/src/styles/styles.scss";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import unet from '../assets/unet.png';
 
 export default function Papers() {
     return (
@@ -7,16 +11,25 @@ export default function Papers() {
              <p className="papers-title">Paper Implementations</p>
 
              <div className="paper-area">
-                <p className="paper-title">End to End Learning for Self-Driving Cars - By Karol Zieba, Nvidia Corporation.</p>
+                <p className="paper-title">End to End Learning for Self-Driving Cars - By Karol Zieba, Nvidia Corporation</p>
+                <AwesomeButtonSocial type="github" className="aws-btn" href='https://github.com/venkateshtata/research'>Github</AwesomeButtonSocial>
                 <p className="paper-desc">A convolutional neural network to map raw pixels from a single front facing camera directly to steering commands in PyTorch. Trained and evaluated the model on comma ai dataset.</p>
-                <p className="paper-desc"> Github : <href link="https://github.com/venkateshtata/research">hello</href></p>
+                
                 <iframe src='https://www.youtube.com/embed/GSU8tp1WxU8'
                                 frameborder='0'
                                 allow='autoplay; encrypted-media'
                                 allowfullscreen
                                 title='video'
                                 className='project-video'
-                            />
+                />
+             </div>
+
+             <div className="paper-area">
+                <p className="paper-title">U-Net: Convolutional Neural Net Architecture for Biomedical Image Segmentation - Florian Schroff, Google Inc.</p>
+                <AwesomeButtonSocial type="github" className="aws-btn" href='https://github.com/venkateshtata/SemanticSegmentation'>Github</AwesomeButtonSocial>
+                <p className="paper-desc">U-Net is a neural network architecture trained end-to-end with a very few images for performing pixel segmentation. I have trained the model on Electron Microscopy Dataset to transform a grayscale EM image of nerve cell into an accurate boundary map differentiating the walls in between, at a pixel level.</p>
+                
+                <img src={unet} width={350} height={280}/>
 
              </div>
         </div>
